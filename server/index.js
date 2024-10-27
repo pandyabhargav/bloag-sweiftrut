1a6e91cad3eb254a64732f1df3e953f029a941d0
pandyabhargav <137371479+pandyabhargav@users.noreply.github.com>
2024-10-27 09:45:52 +0530

    fist comit

--- server/index.js
+++ server/index.js
@@ -3,13 +3,24 @@ import bodyParser from 'body-parser';
 import connectDB from './Config/mongoose.js';
 import router from './Router/blogrouter.js';
 import cors from 'cors';
+import path from 'path';
 
 
 const app = express();
 app.use(bodyParser.json());
+const _diname = path.resolve();
 app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["POST", "GET"],
+    credentials: true,
+}));
 
 app.use('/',router)
+app.use(express.static(path.join(_diname,"/client/dist")));
+app.get('*', (_,res)=>{
+res.sendFile(path.join(_diname,"client","dist","index.html"));
+})
 
 connectDB();
 const port = process.env.PORT;
